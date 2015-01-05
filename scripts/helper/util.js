95e14ebca2a636a1aad724e58e0be8bbbe3dc1c2
dlarbi <dean.m.larbi@gmail.com>
2015-01-04 18:53:47 -0800

    Update util.js
    mouse/keyboard support

--- scripts/helper/util.js
+++ scripts/helper/util.js
@@ -1,16 +1,61 @@
 define(function () {
   return {
     mouseInputOn: function() {
-      window.userClickX = 0;
-      window.userClickY = 0;
+      window.userInputX = 0;
+      window.userInputY = 0;
       function updateMousePosition(evt) {
-        window.userClickX = evt.clientX;
-        window.userClickY = evt.clientY;
+        window.userInputX = evt.clientX;
+        window.userInputY = evt.clientY;
       }
 
       window.addEventListener('mousemove', function mouseMove (evt) {
         updateMousePosition(evt);
       }, false);
+    },
+    keyboardInputOn: function() {
+      window.userInputX = 0;
+      window.userInputY = 0;
+      document.onkeydown = checkKey;
+
+      function checkKey(e) {
+
+        e = e || window.event;
+
+        if (e.keyCode == '38') {
+          // up arrow
+          window.userInputY-=20;
+        }
+        else if (e.keyCode == '40') {
+          window.userInputY+=20;
+        }
+        else if (e.keyCode == '37') {
+          // left arrow
+          window.userInputX-=20;
+        }
+        else if (e.keyCode == '39') {
+          // right arrow
+          window.userInputX+=20;
+        }
+
+      }
+    },
+    findDuplicates : function(arr) {
+      var i,
+      len=arr.length,
+      out=[],
+      obj={};
+
+      for (i=0;i<len;i++) {
+        if (obj[arr[i]] != null) {
+          if (!obj[arr[i]]) {
+            out.push(arr[i]);
+            obj[arr[i]] = 1;
+          }
+        } else {
+          obj[arr[i]] = 0;
+        }
+      }
+      return out;
     }
   };
 });
